5d8e62df58be1ecb856eec645c5da87ce69ae85d
Paulo César Jorge Neto <paulocjneto@gmail.com>
2024-05-25 00:16:37 -0300

    Atualização de Field com o componente Flag

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import params from '../params'
 import Mine from './Mine'
+import Flag from './Flag'
 
 export default props => {
-    const { mined, opened, nearMines, exploded } = props
+    const { mined, opened, nearMines, exploded, flagged } = props
 
     // estilo geral
     const styleField = [styles.field]
@@ -12,7 +13,8 @@ export default props => {
     // Aberta com numeros ou sem
     if (opened) styleField.push(styles.opened)
     if (exploded) styleField.push(styles.exploded)
-    if (styleField.length === 1) styleField.push(styles.regular)
+    if (flagged) styleField.push(styles.flagged)
+    if (!opened && !exploded) styleField.push(styles.regular)
         
     let color = null
     if (nearMines > 0) {
@@ -22,16 +24,20 @@ export default props => {
         if (nearMines >= 6) color = '#F221A9'
     }
 
-
-
     return (
+        <>
         <View style={styleField}>
             {!mined && opened && nearMines > 0 ? 
             <Text style={[styles.label, { color: color }]}> {nearMines} </Text> : false}
 
             {mined && opened ? 
             <Mine/> : false}
+
+            {flagged && !opened ? 
+            <Flag /> : false}
+
         </View>
+        </>
     )
 }
 
